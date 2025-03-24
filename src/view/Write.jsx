9baf8454d9ae1cf04a34dc867bca2ea5dc18bfe7
fallguy <harishvijayan2003@gmail.com>
2025-03-24 20:02:38 +0530

    Complete Function

--- src/view/Write.jsx
+++ src/view/Write.jsx
@@ -1,12 +1,20 @@
     import InputBox from '../Input_box';
     import Button_component from '../Button_component';
+import { useState } from 'react';
     export default function Write({name}){
-        localStorage.setItem("journal","Hey, this is my first Entry");
+        const[content,setContent] = useState("");
+        function handleChange(evt){
+            setContent(evt.target.value);
+        }
+        function handleClick()
+        {
+            localStorage.setItem("journal",content);
+        }
         return(
             <div className='journal'>
             <h1 className='title'>Digital Journal </h1>
-            <InputBox/>
-            <Button_component name="Save"/>
+            <InputBox onChange={handleChange}/>
+            <Button_component name="Save" onClick={handleClick}/>
             <a href="Read"><Button_component name="Read"/></a>
             </div>
             );
