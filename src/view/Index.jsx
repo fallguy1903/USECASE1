b15137f2c723d9a7b039ac04ef446075c5f6dc6f
fallguy <harishvijayan2003@gmail.com>
2025-04-02 16:56:14 +0530

    complete usecase1

--- src/view/Index.jsx
+++ src/view/Index.jsx
@@ -1,10 +1,16 @@
-import Button_component from "../Button_component"
-export default function Index(){
-    return(
-        <>
-            <h1>What do you want to do?</h1>
-            <a href="/Write"><Button_component name="Write"/></a>
-            <a href="Read"><Button_component name="Read"/></a>
-        </>
-    )
+import React, { Component } from "react";
+import Button_component from "../Button_component";
+
+class Index extends Component {
+    render() {
+        return (
+            <>
+                <h1>What do you want to do?</h1>
+                <a href="/Write"><Button_component name="Write" /></a>
+                <a href="/Read"><Button_component name="Read" /></a>
+            </>
+        );
+    }
 }
+
+export default Index;
--- src/view/Write.jsx
+++ src/view/Write.jsx
@@ -1,11 +1,17 @@
     import InputBox from '../Input_box';
     import Button_component from '../Button_component';
-import { useState } from 'react';
+    import { useState,useEffect } from 'react';
     export default function Write({name}){
         const[content,setContent] = useState("");
         function handleChange(evt){
             setContent(evt.target.value);
         }
+        useEffect(() => {
+            const savedJournal = localStorage.getItem("journal");
+            if (savedJournal) {
+                setContent(savedJournal);
+            }
+        }, []);
         function handleClick()
         {
             localStorage.setItem("journal",content);
